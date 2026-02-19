0e9b55e79c3929f66aac070c5ba77e15b3b50369
XINZHANG-ops <1528371521zx@gmail.com>
2026-02-18 21:47:32 -0500

    clear

--- utils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-
-# this function converts string into a hash value
-def hash_string(s):
-    hashed_value = int(hashlib.sha512(s.encode()).hexdigest(), 16)
-    short_hash = hashed_value % (2**32) # if higher values like 64, mapping is not accurate
-    return short_hash
-
